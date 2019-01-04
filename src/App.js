2420ad2cb01ff84b72a18c228f030a4117668b61
Wobblekun <45978900+Wobblekun@users.noreply.github.com>
2019-01-03 21:27:35 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navigation from './Components/Navigation/Navigation.js';
+import Logo from './Components/Logo/Logo.js';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './Components/Rank/Rank.js';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  "particles": {
+    "number": {
+        "value": 160,
+        "density": {
+            "enable": false
+        }
+    },
+    "size": {
+        "value": 20,
+        "random": true
+    },
+    "move": {
+        "direction": "bottom",
+        "out_mode": "out"
+    },
+    "line_linked": {
+        "enable": false
+    }
+},
+"interactivity": {
+    "events": {
+        "onclick": {
+            "enable": true,
+            "mode": "remove"
+        }
+    },
+    "modes": {
+        "remove": {
+            "particles_nb": 1
+        }
+    }
+}}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Particles className='particles'
+          params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* {
+        <FaceRecognition />} */}
       </div>
     );
   }
